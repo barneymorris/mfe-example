84be27cec1bf5b0f0c507bef9cc619d672e00ffd
bsyrpin <bsyrpin@moysklad.ru>
2024-10-22 14:10:47 +0300

    marketing tweaks

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,7 @@
+// Mount function to start up the app
+
+// If we are in developmnt and in isolation,
+// call mount immediately
+
+// We are running throught container
+// and we should export the mount function
