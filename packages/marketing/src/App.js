0d35ae2809c28ccb37b7a1d8e1ac5eddb0614ec3
bsyrpin <bsyrpin@moysklad.ru>
2024-10-23 20:20:04 +0300

    fix deps

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
+import { StylesProvider, ThemeProvider } from "@mui/styles";
+import { createTheme } from "@mui/material";
+
+const theme = createTheme();
 
 export default () => {
   return (
     <div>
       <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/pricing" component={Pricing} />
+              <Route path="/" component={Landing} />
+            </Switch>
+          </BrowserRouter>
+        </ThemeProvider>
       </StylesProvider>
     </div>
   );
